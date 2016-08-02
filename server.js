efad9b7b7c8fc05023cbef69c1675ecfa4ec2544
Daniele <dsabia@hotmail.it>
2016-08-03 01:55:01 +0200

    Customization for openshift

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 var express = require('express');
 var app = express(); 						// create our app w/ express
 var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080; 				// set the port
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var database = require('./config/database'); 			// load the database config
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
@@ -23,5 +26,7 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
-app.listen(port);
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
 console.log("App listening on port " + port);

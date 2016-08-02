6692b680168aa12873011c7461bff36fac291d82
Daniele <dsabia@hotmail.it>
2016-08-03 00:05:14 +0200

    add travis-ci initial configuration and README local instruction

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AngularGridExample
+
+Local path for starting mongoDB:
+``` 
+cd C:\Program Files\MongoDB\Server\3.2\bin 
+mongod --dbpath c:/DB/mongo/test
+```
--- config/database.js
+++ config/database.js
@@ -2,4 +2,4 @@ module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/test'
 };
-//mongod --dbpath c:/DB/mongo/test
+
